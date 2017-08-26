2a7959132291ba923c5c2f36b0aeb071b0e53f4a
zht198484 <zht198484@163.com>
2017-08-26 23:29:11 +0800

    Nio server with multiplexer selector.

---
+++ src/com/zht/nio/MultiplexerNioServer.java
@@ -0,0 +1,105 @@
+package com.zht.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by zht198484 on 2017/8/23.
+ * Nio Server which is based on multiplexer selector
+ */
+public class MultiplexerNioServer implements Runnable {
+    private Selector selector;
+    private ServerSocketChannel serverSocketChannel;
+    private volatile boolean stop;
+
+    public MultiplexerNioServer(int port) {
+        try {
+            selector = Selector.open();
+            serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.configureBlocking(false);
+            serverSocketChannel.socket().bind(new InetSocketAddress(port), 1024);
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("MultiplexerNioServer is started at port " + port);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+    }
+
+    public void stop() {
+        this.stop = true;
+    }
+
+    @Override
+    public void run() {
+        while (!stop) {
+            try {
+                selector.select(1000);
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                SelectionKey selectionKey;
+                while (iterator.hasNext()) {
+                    selectionKey = iterator.next();
+                    iterator.remove();
+                    try {
+                        handleIput(selectionKey);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        if (selectionKey != null) {
+                            selectionKey.cancel();
+                            if (selectionKey.channel() != null) {
+                                selectionKey.channel().close();
+                            }
+                        }
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (selector != null) {
+            try {
+                selector.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleIput(SelectionKey selectionKey) throws IOException {
+        if (selectionKey.isValid()) {
+            if (selectionKey.isAcceptable()) {
+                ServerSocketChannel serverSocketChannel = (ServerSocketChannel) selectionKey.channel();
+                SocketChannel socketChannel = serverSocketChannel.accept();
+                socketChannel.configureBlocking(false);
+                socketChannel.register(selector, SelectionKey.OP_READ);
+            }
+            if (selectionKey.isReadable()) {
+                SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+                ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                int readBytes = socketChannel.read(readBuffer);
+                if (readBytes > 0) {
+                    readBuffer.flip();
+                    byte[] bytes = new byte[readBuffer.remaining()];
+                    readBuffer.get(bytes);
+                    System.out.println("Receive request " + new String(bytes, "UTF-8"));
+                    String response = "Response from nio server";
+                    byte[] responseBytes = response.getBytes();
+                    ByteBuffer writeBuffer = ByteBuffer.allocate(responseBytes.length);
+                    writeBuffer.put(responseBytes);
+                    writeBuffer.flip();
+                    socketChannel.write(writeBuffer);
+                }
+            }
+        }
+    }
+
+
+}
