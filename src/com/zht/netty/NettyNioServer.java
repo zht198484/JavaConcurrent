2c6712478a8f4c3078b65c45b3fb6b2423d05c4c
zht198484 <zht198484@163.com>
2017-08-30 22:48:45 +0800

    Add netty nio server based on netty 4.1

---
+++ src/com/zht/netty/NettyNioServer.java
@@ -0,0 +1,38 @@
+package com.zht.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * Created by zht198484 on 2017/8/30.
+ * Nio server by netty
+ */
+public class NettyNioServer {
+    public void bind(int port) throws InterruptedException {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+
+                            pipeline.addLast(new NettyNioServerHandler());
+                        }
+                    });
+            ChannelFuture channelFuture = serverBootstrap.bind(port).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/com/zht/netty/NettyNioServerHandler.java
@@ -0,0 +1,13 @@
+package com.zht.netty;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+/**
+ * Created by zht198484 on 2017/8/30.
+ * netty nio server handler
+ */
+class NettyNioServerHandler extends ChannelHandlerAdapter {
+
+}
